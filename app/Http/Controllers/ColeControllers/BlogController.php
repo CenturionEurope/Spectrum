12ff284b3e74f86f2ed36efdda5a989e41f52ed5
Peter Day <peterday.main@gmail.com>
2018-07-23 21:30:58 +0100

    Start of blog controller

---
+++ app/Http/Controllers/ColeControllers/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\ColeControllers;
+use App\Http\Controllers\Controller;
+use DB;
+use GrahamCampbell\Markdown\Facades\Markdown;
+
+class BlogController extends Controller
+{
+    
+    // About: Cole DataAction Plugins
+    // Define a plugin below to be used with a DataAction.
+    // DataActions will pass the Data Affected to the Plugin via $Object
+    
+    // ColeDatabaseBoot - Build the database if it does not exist
+	// ColeGetPlugin($Object,$Pre) - Define a plugin when ColeGet is run
+	// ColeCreatePlugin($Object,$Pre) - Define a plugin when data is created
+	// ColeSavePlugin($Object,$Pre) - Define a plugin when data is updated
+	// ColeDeletePlugin($Object,$Pre) - Define a plugin when data is deleted
+	
+ 
+}
