857572bc9b837c082c2934c920e11cba53c5ba47
Peter Day <peterday.main@gmail.com>
2018-08-11 18:21:18 +0100

    Start of cole serial no

---
+++ app/Http/Controllers/Cole/Core/LicenseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Cole\Core;
+use App\Http\Controllers\Controller;
+use DB;
+
+class LicenseController extends Controller
+{
+    public function Check(){
+
+    }
+
+    public function Send(){
+
+    }
+
+    public function Recieve(){
+        
+    }
+
+}
